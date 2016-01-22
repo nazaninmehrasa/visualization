ee7766ce3a289e79b2d79746d6eb23a9b64b4435
Nazanin Mehrasa <nmehrasa@cs-vml-31.cs.sfu.ca>
2016-01-22 14:20:42 -0800

    coloring the selected superpixel with different color

--- coloring1.m
+++ coloring1.m
@@ -1,24 +1,32 @@
-function coloring1( frame_numbers, superPixels)
+function coloring1( frame_numbers, superPixels,sup)
     load('vw_commercial_vidinfo.mat')
     load('/cs/vml2/nmehrasa/eccv16/code/similarities.mat')
     load('allsegsvw_commercial.mat')
-
-
+    [r,c]=find(mapped==sup);
+    disp(r)
    
     for frame = 1:21
         idx=find(frame_numbers==frame);
         if(~isempty(idx))
            % figure;
             img=ones(360,640);
-            img=255.*img;
+            img=0.5.*img;
             sp_temp=superPixels(:,idx);
             is_mem=ismember(labelledlevelvideo(:,:,frame),sp_temp);
-            img=img.*is_mem;
-            img = (cat(3,img,img,img));
-            %imshow(img);
-            imwrite(img,['./coloring3/',num2str(frame),'.jpg']);
+            
+            if(r==frame)
+                [row,col]=find(labelledlevelvideo(:,:,frame)==sup);
+                disp(size(row,1));
+                for i=1:size(row,1)
+                    img(row(i,1),col(i,1))=100;
+                end
+            end
+             img=img.*is_mem;
+             img = (cat(3,img,img,img));
+             imwrite(img,['./coloring4/',num2str(frame),'.jpg']);
         end
     end
+    
     
     
     
